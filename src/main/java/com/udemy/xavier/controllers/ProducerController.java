13c2481b7e93461ca8f93147e9a56b08f8701ebf
xavier_ng <xavierngaba@gmail.com>
2023-02-25 11:42:11 +0100

    projet RabbitMQ-Producer : add conf BDD MySQL + add banner

--- src/main/java/com/udemy/xavier/controllers/ProducerController.java
+++ src/main/java/com/udemy/xavier/controllers/ProducerController.java
@@ -4,7 +4,11 @@ import com.udemy.xavier.entities.StudentJpa;
 import com.udemy.xavier.json.Student;
 import com.udemy.xavier.repository.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.udemy.xavier.producer.IProducer;
 
@@ -24,5 +28,16 @@ public class ProducerController {
         repository.save(etudiant);
         producer.produceMessage(student);
         return student.toString();
+    }
+
+    @PostMapping(path = "/save")
+    public ResponseEntity saveAndSendStudent(@RequestBody Student student) {
+        if(student == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Objet student cannot be null");
+        }
+        StudentJpa etudiant = StudentJpa.builder().nom(student.getNom()).prenom(student.getPrenom()).age(student.getAge()).build();
+        repository.save(etudiant);
+        producer.produceMessage(student);
+        return ResponseEntity.ok(student);
     }
 }
